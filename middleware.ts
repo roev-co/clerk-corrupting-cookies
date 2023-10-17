6517c8fca887c7dc6fdef313a607618c735200f6
Arthur Bodera <abodera@gmail.com>
2023-10-17 14:31:50 +1100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,27 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth: async (auth, req) => {
+    const response = NextResponse.next()
+    response.cookies.set('vercel', 'fast', {
+      sameSite: 'none',
+      secure: true,
+    });
+    response.cookies.set({
+      name: 'vercel-also',
+      value: 'very-fast',
+      path: '/',
+      sameSite: 'none',
+      secure: true,
+    })
+    response.cookies.set('vercel-is', 'very-very-fast', {
+      sameSite: 'none',
+      secure: true,
+    }); 
+    return response;
+  }
 });
 
 export const config = {
