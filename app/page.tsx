25e92c69bf0b7f744fbcbc5dda2eb970d611b1b5
Arthur Bodera <abodera@gmail.com>
2023-10-17 14:58:48 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,40 +8,6 @@ import ShowCookies from './ShowCookies';
 export default function Home() {
   return (
     <main className="">
-      <article className="grid lg:grid-cols-2">
-        <div className="px-8 py-20 md:px-20 lg:py-48">
-          <h1 className="text-5xl font-semibold text-transparent md:text-6xl gradient">
-            Auth starts here.
-          </h1>
-          <p className="mt-2 text-lg">
-            A simple and powerful Next.js template featuring authentication and
-            user management powered by Clerk.
-          </p>
-          <div className="flex gap-2 mt-8">
-            <Link
-              href="/dashboard"
-              className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
-            >
-              View Demo
-              <div className="m-auto">
-                <RightArrow />
-              </div>
-            </Link>
-            <a
-              className="flex gap-2 px-4 py-2 font-semibold text-gray-600 transition duration-100 rounded-lg hover:text-gray-800"
-              href="#features"
-            >
-              Learn more
-              <div className="m-auto">
-                <DownArrow />
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center">
-          <Image src={componentsImg} alt="Clerk embeddable components" />
-        </div>
-      </article>
       <article
         className="px-8 py-12 bg-black bg-opacity-5 md:px-20 md:py-24"
         id="cookies"
